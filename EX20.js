19628d4530a4cfd5dfd231dc3077bdd6be25f3c6
Myiah Moore <myiahsade@gmail.com>
2018-07-31 15:12:49 -0500

    check in

---
+++ EX20.js
@@ -0,0 +1,10 @@
+function main()
+{
+    var counter = -10;
+    while (counter < 5){
+        alert(counter);
+        counter = counter + 1;
+    }
+}
+
+main();
---
+++ EX21.js
@@ -0,0 +1,10 @@
+function main()
+{
+    var counter = 0;
+    while (counter < 100){
+        console.log(counter);
+        counter = counter + 2;
+    }
+}
+
+main();
